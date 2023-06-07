16368576d648966b75fc50c6b384ee065cbe04a5
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-07 13:48:56 +0200

    Added an interface that represent how to convert a class to a Dto of that class

---
+++ EventPlatform.Entities/Interfaces/IDtoConversion.cs
@@ -0,0 +1,8 @@
+﻿namespace EventPlatform.Entities.Interfaces;
+
+public interface IDtoConversion<in TFrom, out TTo>
+    where TFrom : class, IDtoConversion<TFrom, TTo>
+    where TTo : class, IDto<TTo>
+{
+    TTo ToDto();
+}
