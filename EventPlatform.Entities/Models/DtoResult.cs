4c461dd6208adadfefcf81cb7c07d56728bfec12
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-07 13:50:20 +0200

    Added a base class that represents an API result for a Dto

---
+++ EventPlatform.Entities/Models/DtoResult.cs
@@ -0,0 +1,29 @@
+﻿using EventPlatform.Entities.Enums;
+using EventPlatform.Entities.Interfaces;
+using System.Text.Json.Serialization;
+
+namespace EventPlatform.Entities.Models;
+
+public abstract class DtoResult<TClass, TDto>
+    where TClass : class, IDtoConversion<TClass, TDto>
+    where TDto : class, IDto<TDto>
+{
+    protected readonly TDto _dto;
+    protected readonly Status _status;
+
+    [JsonIgnore]
+    public TDto Dto => _dto;
+    public Status Status => _status;
+
+    public DtoResult(TClass c, Status status)
+    {
+        _dto = c.ToDto();
+        _status = status;
+    }
+
+    public DtoResult(TDto dto, Status status)
+    {
+        _dto = dto;
+        _status = status;
+    }
+}
