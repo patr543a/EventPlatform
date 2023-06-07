4a53bf14216ff7cde05922f957577a475f8db24c
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-07 13:36:14 +0200

    Added ServiceBase.cs

---
+++ EventPlatform.Api/Classes/ServiceBase.cs
@@ -0,0 +1,9 @@
+﻿using EventPlatform.Api.Interfaces;
+using EventPlatform.DataAccess.Classes;
+
+namespace EventPlatform.Api.Classes;
+
+public class ServiceBase
+{
+    protected static readonly UnitOfWork _repositories = new();
+}
