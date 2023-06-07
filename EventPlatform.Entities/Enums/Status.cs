7cfb5f289b2ab3c261b53ff129a52ff1358d0d8c
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-07 13:47:07 +0200

    Added Status enum

---
+++ EventPlatform.Entities/Enums/Status.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EventPlatform.Entities.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum Status
+{
+    None = 0,
+    Success,
+    AccessDenied,
+    InvalidObject,
+}
