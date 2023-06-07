8bc13283774e1b3e718114b8e8459c2eb9d5b2db
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-07 13:44:31 +0200

    Added a constructor

--- EventPlatform.Entities/DTO/EventDto.cs
+++ EventPlatform.Entities/DTO/EventDto.cs
@@ -1,15 +1,27 @@
-﻿namespace EventPlatform.Entities.DTO
+﻿using EventPlatform.Entities.ECP;
+using EventPlatform.Entities.Interfaces;
+
+namespace EventPlatform.Entities.DTO;
+
+public class EventDto
+    : IDto<EventDto>
 {
-    public class EventDto
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public bool NeedsVolunteers { get; set; }
+
+    public string Organizer { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public EventDto(Event @event)
     {
-        public DateTime StartDate { get; set; }
-
-        public DateTime EndDate { get; set; }
-
-        public bool NeedsVolunteers { get; set; }
-
-        public string Organizer { get; set; } = null!;
-
-        public string Description { get; set; } = null!;
+        StartDate = @event.StartDate;
+        EndDate = @event.EndDate;
+        NeedsVolunteers = @event.NeedsVolunteers;
+        Organizer = @event.OrganizerIdFk;
+        Description = @event.Description;
     }
 }
--- EventPlatform.Entities/DTO/TaskDto.cs
+++ EventPlatform.Entities/DTO/TaskDto.cs
@@ -1,7 +1,17 @@
-﻿namespace EventPlatform.Entities.DTO
-{
-    public class TaskDto
-    {
+﻿using EventPlatform.Entities.Interfaces;
 
+namespace EventPlatform.Entities.DTO;
+
+public class TaskDto
+    : IDto<TaskDto>
+{
+    public string Description { get; set; } = null!;
+
+    public EventDto Event { get; set; } = null!;
+
+    public TaskDto(ECP.Task task)
+    {
+        Description = task.Description;
+        Event = new(task.Event);
     }
 }
