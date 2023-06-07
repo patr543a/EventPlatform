886270ad4299a8a221c791bc6b7c85d5aa8775fc
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-07 13:36:02 +0200

    Added CustomController.cs

---
+++ EventPlatform.Api/Classes/CustomController.cs
@@ -0,0 +1,14 @@
+﻿using EventPlatform.Api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlatform.Api.Classes;
+
+public class CustomController<TService>
+    : ControllerBase
+    where TService : IService
+{
+    protected readonly TService _service;
+
+    public CustomController(TService service)
+        => _service = service;
+}
