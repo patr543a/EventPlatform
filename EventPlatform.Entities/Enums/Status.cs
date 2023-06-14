aaa3b5e544fc07ab8dd128496a356dfef46a1484
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-14 13:56:55 +0200

    Added a new status and gave them descriptions

--- EventPlatform.Entities/Enums/Status.cs
+++ EventPlatform.Entities/Enums/Status.cs
@@ -1,13 +1,26 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace EventPlatform.Entities.Enums;
 
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Status
 {
+    [EnumMember(Value = "Server couldn't give a status")]
     None = 0,
+
+    [EnumMember(Value = "Success")]
     Success,
+
+    [EnumMember(Value = "Access was denied")]
     AccessDenied,
+
+    [EnumMember(Value = "Body was invalid")]
     InvalidObject,
+
+    [EnumMember(Value = "Resource not found")]
     NotFound,
+
+    [EnumMember(Value = "Failed due to data already existing")]
+    AlreadyPresent,
 }
