9da7d8cea57fbec043cedaa54e4c8f15fd8a4048
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-15 14:21:07 +0200

    Added more methods to interface

--- EventPlatform.Api/Interfaces/IUserService.cs
+++ EventPlatform.Api/Interfaces/IUserService.cs
@@ -9,6 +9,11 @@ public interface IUserService
     : IService
 {
     IEnumerable<TaskDto> GetUserTaskHistory(string username);
+    IEnumerable<UserDto> GetCandidatesOfEvent(Guid sessionToken, int eventId);
+    IEnumerable<UserDto> GetVolunteersOfTask(Guid sessionToken, int taskId);
+    IEnumerable<TaskDto> GetTasksUserVolunteerFor(Guid sessionToken, string username, int eventId);
     PutResult<User, UserDto> UpdateUserDescription(string username, string description);
     PutResult<User, UserDto> UpdateUserCandidate(string username, int taskId);
+    PutResult<User, UserDto> UpdateUserAssignment(Guid sessionToken, string username, int taskId);
+    DeleteResult<User, UserDto> DeleteUserAssignment(Guid sessionToken, string username, int taskId);
 }
