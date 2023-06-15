58b88038bce951e658c402935dae83469db20688
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-15 14:21:19 +0200

    Added more methods to service

--- EventPlatform.Api/Services/UserService.cs
+++ EventPlatform.Api/Services/UserService.cs
@@ -5,6 +5,7 @@ using EventPlatform.Entities.ECP;
 using EventPlatform.Entities.Models;
 using EventPlatform.Entities.Enums;
 using Task = EventPlatform.Entities.ECP.Task;
+using EventPlatform.DataAccess.Classes;
 
 namespace EventPlatform.Api.Services;
 
@@ -17,6 +18,21 @@ public class UserService
             .GetUserTaskHistory(username)
             select new TaskDto(t);
 
+    public IEnumerable<UserDto> GetCandidatesOfEvent(Guid sessionToken, int eventId)
+        => _repositories
+            .UserRepository
+            .GetCandidatesWithIdentification(sessionToken, new Event() { EventId = eventId });
+
+    public IEnumerable<UserDto> GetVolunteersOfTask(Guid sessionToken, int taskId)
+        => _repositories
+            .UserRepository
+            .GetVolunteersOfTaskWithIdentification(sessionToken, new Task() { TaskId = taskId });
+
+    public IEnumerable<TaskDto> GetTasksUserVolunteerFor(Guid sessionToken, string username, int eventId)
+        => _repositories
+            .TaskRepository
+            .GetTasksUserVolunteerForWithIdentification(sessionToken, username, new Event() { EventId = eventId });
+
     public PutResult<User, UserDto> UpdateUserDescription(string username, string description)
         => _repositories
             .UserRepository
@@ -24,11 +40,31 @@ public class UserService
 
     public PutResult<User, UserDto> UpdateUserCandidate(string username, int taskId)
     {
-        var user = _repositories.UserRepository.Get(u => u.UserId == username, null, "TaskIdCandidate").FirstOrDefault();
+        var user = _repositories.UserRepository.GetByID(username);
 
         if (user is null)
             return new PutResult<User, UserDto>(new User(), Status.NotFound);
 
         return _repositories.TaskRepository.UpdateTaskCandidate(taskId, user);
+    }
+
+    public PutResult<User, UserDto> UpdateUserAssignment(Guid sessionToken, string username, int taskId)
+    {
+        var user = _repositories.UserRepository.GetByID(username);
+
+        if (user is null)
+            return new PutResult<User, UserDto>(new User(), Status.NotFound);
+
+        return _repositories.TaskRepository.UpdateTaskAssignment(sessionToken, taskId, user);
+    }
+
+    public DeleteResult<User, UserDto> DeleteUserAssignment(Guid sessionToken, string username, int taskId)
+    {
+        var user = _repositories.UserRepository.GetByID(username);
+
+        if (user is null)
+            return new DeleteResult<User, UserDto>(new User(), Status.NotFound);
+
+        return _repositories.TaskRepository.DeleteTaskAssignment(sessionToken, taskId, user);
     }
 }
