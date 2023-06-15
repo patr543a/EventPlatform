54fa5c50b020f89930345e61628119553bc17004
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-15 14:20:54 +0200

    Added more methods

--- EventPlatform.Api/Controllers/UserController.cs
+++ EventPlatform.Api/Controllers/UserController.cs
@@ -23,15 +23,56 @@ public class UserController
 
     [HttpGet]
     [Route("getUserTaskHistory")]
-    public async Task<ActionResult<IEnumerable<TaskDto>>> GetUserTaskHistory(Guid? sessionToken, string? username)
+    public async Task<ActionResult<IEnumerable<TaskDto>>> GetUserTaskHistory(Guid? sessionToken)
     {
-        if (username is null || sessionToken is null)
+        if (sessionToken is null)
+            return BadRequest("Missing parameters");
+
+        var username = LoginHandler.GetUsername((Guid)sessionToken);
+
+        if (username is null)
+            return Unauthorized("Access denied");
+
+        return Ok(await Task.FromResult(_service.GetUserTaskHistory(username)));
+    }
+
+    [HttpGet]
+    [Route("getCandidatesOfEvent")]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetCandidatesOfEvent(Guid? sessionToken, int? eventId)
+    {
+        if (sessionToken is null || eventId is null)
             return BadRequest("Missing parameters");
 
         if (LoginHandler.GetUserPermissions((Guid)sessionToken) < UserType.Organizer)
             return Unauthorized("Access denied");
 
-        return Ok(await Task.FromResult(_service.GetUserTaskHistory(username)));
+        return Ok(await Task.FromResult(_service.GetCandidatesOfEvent((Guid)sessionToken, (int)eventId)));
+    }
+
+    [HttpGet]
+    [Route("getVolunteersOfTask")]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetVolunteersOfTask(Guid? sessionToken, int? taskId)
+    {
+        if (sessionToken is null || taskId is null)
+            return BadRequest("Missing parameters");
+
+        if (LoginHandler.GetUserPermissions((Guid)sessionToken) < UserType.Organizer)
+            return Unauthorized("Access denied");
+
+        return Ok(await Task.FromResult(_service.GetVolunteersOfTask((Guid)sessionToken, (int)taskId)));
+    }
+
+    [HttpGet]
+    [Route("getTasksUserVolunteerFor")]
+    public async Task<ActionResult<IEnumerable<TaskDto>>> GetTasksUserVolunteerFor(Guid? sessionToken, string? username, int? eventId)
+    {
+        if (sessionToken is null || username is null || eventId is null)
+            return BadRequest("Missing parameters");
+
+        if (LoginHandler.GetUserPermissions((Guid)sessionToken) < UserType.Organizer)
+            return Unauthorized("Access denied");
+
+        return Ok(await Task.FromResult(_service.GetTasksUserVolunteerFor((Guid)sessionToken, username, (int)eventId)));
     }
 
     [HttpPut]
@@ -62,5 +103,31 @@ public class UserController
             return Unauthorized("Access denied");
 
         return Ok(await Task.FromResult(_service.UpdateUserCandidate(username, (int)taskId)));
+    }
+
+    [HttpPut]
+    [Route("updateUserAssignment")]
+    public async Task<ActionResult<PutResult<User, UserDto>>> UpdateUserAssignment(Guid? sessionToken, string? username, int? taskId)
+    {
+        if (sessionToken is null || username is null || taskId is null)
+            return BadRequest("Missing parameters");
+
+        if (LoginHandler.GetUserPermissions((Guid)sessionToken) < UserType.Organizer)
+            return Unauthorized("Access denied");
+
+        return Ok(await Task.FromResult(_service.UpdateUserAssignment((Guid)sessionToken, username, (int)taskId)));
+    }
+
+    [HttpDelete]
+    [Route("deleteUserAssignment")]
+    public async Task<ActionResult<DeleteResult<User, UserDto>>> DeleteUserAssignment(Guid? sessionToken, string? username, int? taskId)
+    {
+        if (sessionToken is null || username is null || taskId is null)
+            return BadRequest("Missing parameters");
+
+        if (LoginHandler.GetUserPermissions((Guid)sessionToken) < UserType.Organizer)
+            return Unauthorized("Access denied");
+
+        return Ok(await Task.FromResult(_service.DeleteUserAssignment((Guid)sessionToken, username, (int)taskId)));
     }
 }
